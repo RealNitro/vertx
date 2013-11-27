cf637e6f199954fd592897c4a6fdb7b1005a73de
SaschaMoellering <sascha.moellering@zanox.com>
2013-11-27 16:23:53 +0100

    Version number increased

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sascha.moellering@zanox.com'
 license          'All rights reserved'
 description      'Installs/Configures vertx 2.0.2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
 
 depends "ark"
 depends "java"
