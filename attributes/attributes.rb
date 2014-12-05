d3fadb9940140ad4706e583e91d93a831bb26093
Sander van de Graaf <mail@svdgraaf.nl>
2014-12-05 09:29:37 +0100

    made pid file and status file location configurable

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -5,7 +5,9 @@
 # Author 2013 Sascha Möllering (sascha.moellering@zanox.com)
 # 
 
-default[:vertx][:version]    = '2.1.5'
-default[:vertx][:url]        = 'https://bintray.com/artifact/download/vertx/downloads/vert.x-2.1.5.tar.gz'
-default[:vertx][:home]       = '/srv/vertx'
-default[:vertx][:module]     = ''
+default[:vertx][:version]       = '2.1.5'
+default[:vertx][:url]           = 'https://bintray.com/artifact/download/vertx/downloads/vert.x-2.1.5.tar.gz'
+default[:vertx][:home]          = '/srv/vertx'
+default[:vertx][:module]        = ''
+default[:vertx][:pid_file]      = '/var/run/vertx.pid'
+default[:vertx][:status_file]   = '/var/www/monitor/status.txt'
